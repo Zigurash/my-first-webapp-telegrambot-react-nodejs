5e28150e83a6a72c7417346b49d10bcdd946240f
Zigurash <haus42874287@gmail.com>
2025-06-21 13:28:07 +0700

    4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '7365341531:AAH5LDCX-EGpOkAwAhHzcUDz2qSDTwIqu4s';
-const webAppUrl = 'https://jolly-biscotti-4d04f2.netlify.app/'
+const webAppUrl = 'https://jolly-biscotti-4d04f2.netlify.app'
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
