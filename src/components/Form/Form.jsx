c40fdab97dffaef755eadd08e7d34402bcbf2442
Zigurash <haus42874287@gmail.com>
2025-06-21 12:57:23 +0700

    4

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,62 @@
-import React from "react";      
+import React, { useState } from "react";      
 import './Form.css';
+import react from "react";
 
 
-const Form= (props) => {
+const Form = () => {
+
+    const [country, setCountry] = useState('');
+    const [city, setCity] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeCity = (e) => {
+        setCity(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubjetc = (e) => {
+        setSubject(e.target.value)
+    }
+ 
     return (
-    <div> FORM </div>
+    <div className="form"> 
+        <h3>Введите ваши данные</h3>
+        <input
+            className={'input'}
+            type="text"
+            placeholder={'Страна'}
+            value={country}
+            onChange={onChangeCountry}
+        />
+        <input
+            className={'input'}
+            type="text"
+            placeholder={'Город'}
+            value={city}
+            onChange={onChangeCity}
+        />        
+        <input
+            className={'input'}
+            type="text"
+            placeholder={'Улица'}
+            value={street}
+            onChange={onChangeStreet}
+        />
+        
+        <select value={subject} onChange={onChangeSubjetc} className={'select'}>
+            <option value={'physical'}>Физ. лицо</option>
+             <option value={'legal'}>Юр. лицо</option>
+        </select>
+
+        </div>
 
     );
 }
